f676baea80e84ec91c20d2bd09815bbe8178c948
Tassio <tassiodemeneses@gmail.com>
2019-12-16 20:38:10 -0300

    Update SecurityUtil.java

--- src/main/java/com/example/seguranca/util/SecurityUtil.java
+++ src/main/java/com/example/seguranca/util/SecurityUtil.java
@@ -1,9 +1,1 @@
-//package com.example.seguranca.util;
-//
-//public class SecurityUtil {
-//
-//    public static boolean isArceUser(String email) {
-//        return email.contains("@arce.ce.gov.br");
-//    }
-//
-//}
+
